35962d0f56c6011d8ac3c7f7722740c197ebf177
muchenqing <muchen_ovo@outlook.com>
2026-01-20 22:57:58 +0800

    Initial commit

---
+++ .vitepress/config.mjs
@@ -0,0 +1,59 @@
+import { defineConfig } from 'vitepress'
+import { set_sidebar } from "./utils/auto_sidebar.mjs";	// 改成自己的路径
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  base:"/QL/",
+  head: [["link", { rel: "icon", href: "/QL//qing.jpg" }]],
+  title: "沐辰的简易仓库",
+  description: "A VitePress Site",
+  themeConfig: {
+    outlineTitle:"目录",
+    outline:[2,6],
+    logo: "/qing.jpg", // 配置logo位置，public目录
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text:'首页',link:'/'},
+      { text: 'MirlKoi', items:[
+        {text:'MirlKoi',link:'https://cnmiw.com/'},
+      ] },
+      { text: '视频/番', items:[
+        {text:'哔哩哔哩',link:'https://www.bilibili.com/'},
+        {text:'Web Emby(在线番剧) ',link:'https://cnmiw.com/acg/Daisysg.html'},
+      ] },
+      { text: '自动生成侧边栏', link: '/front-end/react/' },
+      { text: '自动生成侧边栏2', link: '/backend/rabbitmq' },
+      
+    ],
+
+    sidebar: false, // 关闭侧边栏
+    aside: "left", // 设置右侧侧边栏在左侧显示
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/muchenqing' }
+    ],
+    footer:{
+      copyright:"Copyright @ 2026 Lan Qing"
+    },
+       // 设置搜索框的样式
+    search: {
+      provider: "local",
+      options: {
+        translations: {
+          button: {
+            buttonText: "搜索文档",
+            buttonAriaLabel: "搜索文档",
+          },
+          modal: {
+            noResultsText: "无法找到相关结果",
+            resetButtonTitle: "清除查询条件",
+            footer: {
+              selectText: "选择",
+              navigateText: "切换",
+            },
+          },
+        },
+      },
+    },
+  },
+  
+})
---
+++ index.md
@@ -0,0 +1,31 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "沐辰的简易仓库"
+  text: "仓库"
+  tagline: In My Barren Land,You Are My Only White Rose
+  actions:
+    - theme: brand
+      text: 哔哩哔哩
+      link: https://www.bilibili.com/
+    - theme: brand
+      text: MirlKoi
+      link: https://cnmiw.com/
+    - theme: brand
+      text: Web Emby（在线番剧) 
+      link: https://cnmiw.com/acg/Daisysg.html
+    - theme: alt
+      text: API 实例
+      link: /api-examples
+
+features:
+  - title: Feature A
+    details: 特征1的详细描述
+  - title: Feature B
+    details: 特征2的详细描述
+  - title: Feature C
+    details: 特征3的详细描述
+---
+
